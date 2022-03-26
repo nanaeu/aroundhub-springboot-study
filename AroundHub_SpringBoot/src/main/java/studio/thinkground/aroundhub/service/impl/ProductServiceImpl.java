45055295fc8a89a2538f30a9752e70e4372c164b
Yujin <lyubliss@gmail.com>
2022-03-26 19:01:49 +0900

    edit: @Service 애노테이션 누락

--- AroundHub_SpringBoot/src/main/java/studio/thinkground/aroundhub/service/impl/ProductServiceImpl.java
+++ AroundHub_SpringBoot/src/main/java/studio/thinkground/aroundhub/service/impl/ProductServiceImpl.java
@@ -1,11 +1,13 @@
 package studio.thinkground.aroundhub.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import studio.thinkground.aroundhub.data.dto.ProductDto;
 import studio.thinkground.aroundhub.data.entity.Product;
 import studio.thinkground.aroundhub.data.handler.ProductDataHandler;
 import studio.thinkground.aroundhub.service.ProductService;
 
+@Service
 public class ProductServiceImpl implements ProductService {
 
     private ProductDataHandler productDataHandler;
