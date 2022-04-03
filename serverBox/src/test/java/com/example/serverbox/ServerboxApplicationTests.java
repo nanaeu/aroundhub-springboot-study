59cdd00435e6bbd1d735327df44ea00bac0af37a
Yujin <lyubliss@gmail.com>
2022-04-04 01:39:27 +0900

    add: Week 04 - serverBox

---
+++ serverBox/src/test/java/com/example/serverbox/ServerboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.serverbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerboxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
